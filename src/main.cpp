337ab75d93635b1bea1df9adbda0deb3d0c3012a
ldas <l.bispo@live.com>
2022-11-05 16:09:05 +0100

    camshift

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,79 @@
-#include <opencv2/core/utility.hpp>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/tracking.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
-#include <iostream>
-#include <cstring>
-
-// https://docs.opencv.org/4.6.0/d2/d0a/tutorial_introduction_to_tracker.html
-
-using namespace std;
+#include <opencv2/video.hpp>
 using namespace cv;
+using namespace std;
+
+// https://docs.opencv.org/4.x/d7/d00/tutorial_meanshift.html
 
 int main(int argc, char **argv)
 {
-    // show help
-    if (argc < 2)
+    const string about =
+        "This sample demonstrates the camshift algorithm.\n"
+        "The example file can be downloaded from:\n"
+        "  https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4";
+    const string keys =
+        "{ h help |      | print this help message }"
+        "{ @image |<none>| path to image file }";
+    CommandLineParser parser(argc, argv, keys);
+    parser.about(about);
+    if (parser.has("help"))
     {
-        cout << " Usage: tracker <video_name>\n"
-                " examples:\n"
-                " example_tracking_kcf Bolt/img/%04d.jpg\n"
-                " example_tracking_kcf faceocc2.webm\n"
-             << endl;
+        parser.printMessage();
         return 0;
     }
-    // declares all required variables
-    Rect roi;
-    Mat frame;
-    // create a tracker object
-    Ptr<Tracker> tracker = TrackerKCF::create();
-    // set input video
-    std::string video = argv[1];
-    VideoCapture cap(video);
-    // get bounding box
-    cap >> frame;
-    roi = selectROI("tracker", frame);
-    // quit if ROI was not selected
-    if (roi.width == 0 || roi.height == 0)
+    string filename = parser.get<string>("@image");
+    if (!parser.check())
+    {
+        parser.printErrors();
         return 0;
-    // initialize the tracker
-
-    printf("%d, %d, %d, %d\r\n",roi.x, roi.y, roi.width, roi.height);
-
-    tracker->init(frame, roi);
-    // perform the tracking process
-    printf("Start the tracking process, press ESC to quit.\n");
-
-    const string source = argv[1];                                   // the source file name
-    string::size_type pAt = source.find_last_of('.');                // Find extension point
-    const string NAME = source.substr(0, pAt) + argv[2][0] + ".avi"; // Form the new name with container
-    int ex = static_cast<int>(cap.get(CAP_PROP_FOURCC));             // Get Codec Type- Int form
-
-    // Transform from int to char via Bitwise operators
-    char EXT[] = {(char)(ex & 0XFF), (char)((ex & 0XFF00) >> 8), (char)((ex & 0XFF0000) >> 16), (char)((ex & 0XFF000000) >> 24), 0};
-
-    Size S = Size((int)cap.get(CAP_PROP_FRAME_WIDTH), // Acquire input size
-                  (int)cap.get(CAP_PROP_FRAME_HEIGHT));
-    VideoWriter outputVideo; // Open the output
-
-    outputVideo.open(NAME, ex, cap.get(CAP_PROP_FPS), S, true);
-
-    if (!outputVideo.isOpened())
-    {
-        cout << "Could not open the output video for write: " << source << endl;
-        return -1;
     }
-    cout << "Input frame resolution: Width=" << S.width << "  Height=" << S.height
-         << " of nr#: " << cap.get(CAP_PROP_FRAME_COUNT) << endl;
-    cout << "Input codec type: " << EXT << endl;
-
-    for (;;)
+    VideoCapture capture(filename);
+    if (!capture.isOpened())
     {
-        // get frame from the video
-        cap >> frame;
-        // stop the program if no more images
-        if (frame.rows == 0 || frame.cols == 0)
+        // error in opening the video input
+        cerr << "Unable to open file!" << endl;
+        return 0;
+    }
+    Mat frame, roi, hsv_roi, mask;
+    // take first frame of the video
+    capture >> frame;
+    // setup initial location of window
+    Rect track_window(636, 356, 30, 30); // simply hardcoded the values
+    // set up the ROI for tracking
+    roi = frame(track_window);
+    cvtColor(roi, hsv_roi, COLOR_BGR2HSV);
+    inRange(hsv_roi, Scalar(0, 60, 32), Scalar(180, 255, 255), mask);
+    float range_[] = {0, 180};
+    const float *range[] = {range_};
+    Mat roi_hist;
+    int histSize[] = {180};
+    int channels[] = {0};
+    calcHist(&hsv_roi, 1, channels, mask, roi_hist, 1, histSize, range);
+    normalize(roi_hist, roi_hist, 0, 255, NORM_MINMAX);
+    // Setup the termination criteria, either 10 iteration or move by at least 1 pt
+    TermCriteria term_crit(TermCriteria::EPS | TermCriteria::COUNT, 10, 5);
+    while (true)
+    {
+        Mat hsv, dst;
+        capture >> frame;
+        if (frame.empty())
             break;
-        // update the tracking result
-        tracker->update(frame, roi);
-        // draw the tracked object
-        rectangle(frame, roi, Scalar(255, 0, 0), 2, 1);
-        // show image with the tracked object
-        imshow("tracker", frame);
-        outputVideo.write(frame);
-        // Write frame to video output
-        // quit on ESC button
-        if (waitKey(1) == 27)
+        cvtColor(frame, hsv, COLOR_BGR2HSV);
+        calcBackProject(&hsv, 1, channels, roi_hist, dst, range);
+        // apply camshift to get the new location
+        RotatedRect rot_rect = CamShift(dst, track_window, term_crit);
+        // Draw it on image
+        Point2f points[4];
+        rot_rect.points(points);
+        for (int i = 0; i < 4; i++)
+            line(frame, points[i], points[(i + 1) % 4], 255, 2);
+        imshow("img2", frame);
+        int keyboard = waitKey(300);
+        if (keyboard == 'q' || keyboard == 27)
             break;
     }
-    return 0;
 }
